aa2258c402372fcaca27b4d36bc245129f8e6439
lorsumalpha <artmannchristian9@gmail.com>
2023-02-01 08:53:03 +0100

    Komposition und Aggregation

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,35 @@
 #include "src/classes/Zauberer/Zauberer.h"
 #include "src/classes/Krieger/Krieger.h"
 
+// Komposition: "Muss" Beziehung - etwas das zur Funktionalität  benötigt wird
+// Aggregation: "Weiche" Beziehung - kann ausgetauscht werden oder in anderen Objekten wiederverwendet werden
+
 using namespace std;
 
+class Spieler{
+    string Name;
+};
+
+class Menschaergerdichnicht{
+    Spieler spielFigur[3];
+};
+
+class Netzteil{
+    int Leistung;
+};
+
+class CPU {
+    int Taktrate;
+};
+
+class PC {
+    string Hersteller;
+    string Modell;
+    float Stromverbrauch;
+    CPU Intel;
+    Netzteil beQuiet;
+};
+
 int main() {
-    Zauberer Merlin;                    // Instanz einer Klasse, Objekt
-    Krieger Mark;
-    Mark.angriff();
-    Merlin.zaubern();
-    Merlin.sterben();
-    return 0;
+
 }
